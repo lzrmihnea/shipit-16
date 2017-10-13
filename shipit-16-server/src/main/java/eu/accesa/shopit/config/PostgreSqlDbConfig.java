7a1f5973862e3f2689f1d98b813049d8b6c60f88
Mihnea Lazar <lzr_mihnea@yahoo.com>
2017-10-14 00:12:29 +0300

    [server][infrastructure] added swagger

--- shipit-16-server/src/main/java/eu/accesa/shopit/config/PostgreSqlDbConfig.java
+++ shipit-16-server/src/main/java/eu/accesa/shopit/config/PostgreSqlDbConfig.java
@@ -7,10 +7,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile({"!integration-test"})
 @MapperScan(basePackages = "eu.accesa.shopit.repository")
 public class PostgreSqlDbConfig {
 
