c0df5a5a67de00de2481c81aa495fc5597651ddb
U-DOM_MEDIASAT\gellerth <gellerth@mediamarktsaturn.com>
2017-10-14 01:52:17 +0300

    Jpa method queries

--- shipit-16-server/src/main/java/eu/accesa/shopit/repository/ShoppingListRepository.java
+++ shipit-16-server/src/main/java/eu/accesa/shopit/repository/ShoppingListRepository.java
@@ -3,6 +3,13 @@ package eu.accesa.shopit.repository;
 import eu.accesa.shopit.entity.ShoppingList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, Integer> {
 
+    List<ShoppingList> findByProductsIn(String product);
+    List<ShoppingList> findByDate(Date product);
+
+    ShoppingList findFirstByProductsInOOrderByDateDesc(String product);
 }
